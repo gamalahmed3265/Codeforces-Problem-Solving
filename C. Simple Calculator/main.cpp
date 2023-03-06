31bbb54138a58792c666440bea44070fc6f72a67
gamal ahmed <gamalahmed3265@gmail.com>
2023-03-06 22:31:03 +0200

    add C. Simple Calculator

---
+++ C. Simple Calculator/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<" X + Y = summation result\n" <<
+        "X * Y = multiplication result \n" <<
+        "X - Y = subtraction result\n";
+    long long int fn, ln;
+    cin >> fn>>ln;
+    cout<< fn <<" + "<< ln<<" = " <<  fn + ln << endl;
+    cout<< fn <<" * "<< ln<<" = " <<  fn * ln << endl;
+    cout<< fn <<" - "<< ln<<" = " <<  fn - ln << endl;
+}
