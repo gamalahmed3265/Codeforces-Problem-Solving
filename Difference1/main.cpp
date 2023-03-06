6e0bcc3d69514a78bf337656531e23987e148b1c
gamal ahmed <gamalahmed3265@gmail.com>
2023-03-06 22:43:37 +0200

    add Difference1

---
+++ Difference1/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long int a, b,c,d;
+    cin >> a>>b>>c>>d;
+    long long x = (a * b) - (c * d);
+    cout<<"Difference = " << x << endl;
+}
