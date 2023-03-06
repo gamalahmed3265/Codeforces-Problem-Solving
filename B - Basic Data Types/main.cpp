9b93a28143dcaa5799f8cfe0c70c84adc6174d9c
gamal ahmed <gamalahmed3265@gmail.com>
2023-03-06 22:10:23 +0200

    add B - Basic Data Types

---
+++ B - Basic Data Types/main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    long long int li;
+    char ch;
+    float f;
+    double d;
+
+    cin >>i >>li>> ch >>f >>d;
+    cout << i<<endl;
+    cout << li << endl;
+    cout << ch << endl;
+    cout << f << endl;
+    cout << d << endl;
+}
