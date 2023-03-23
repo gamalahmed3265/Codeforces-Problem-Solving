a1860f84404d4e5b1cf10c583576eb4be3e9004d
gamal ahmed <gamalahmed3265@gmail.com>
2023-03-23 17:26:42 +0200

    ad D - Fixed Password ICPC Assiut University Community

---
+++ D-Fixed Password/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i ,el;
+    while(true){
+         cin>>el;
+        if(el==1999){
+             cout<<"Correct"<<endl;
+             break;
+        }
+        else{
+             cout<<"Wrong"<<endl;
+        }
+    }
+  
+    
+    return 0;
+}
