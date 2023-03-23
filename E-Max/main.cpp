29bfa22c79a347dc760f643f1831f7f41857bcd1
gamal ahmed <gamalahmed3265@gmail.com>
2023-03-23 17:42:30 +0200

    ad E-Max ICPC Assiut University Community

---
+++ E-Max/main.cpp
@@ -0,0 +1,16 @@
+#include iostream
+using namespace std;
+int main() {
+    int n,i;
+    cinn;
+    int max=0;
+    int arr[n];
+    for(i=0; in; i++){
+        cinarr[i];
+        if(arr[i]max)
+              max=arr[i];
+    }
+    coutmaxendl;
+    
+    return 0;
+}
