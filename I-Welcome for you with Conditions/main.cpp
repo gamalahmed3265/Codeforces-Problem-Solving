eab9ac343a9614a35d771e29e671eadc909a88cb
gamal ahmed <gamalahmed3265@gmail.com>
2023-03-30 14:37:50 +0200

    add I-Welcome for you with Conditions ICPC

---
+++ I-Welcome for you with Conditions/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+#include <cmath>
+
+int main(){
+    int num1, num2;
+	cin >> num1 >> num2;
+	if (num1 >= num2)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+		
+	return 0;
+}
