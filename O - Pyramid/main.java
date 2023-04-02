73e9bc1844a8f1e3799d51b74349d494aef84fd7
gamal ahmed <gamalahmed3265@gmail.com>
2023-04-02 09:41:15 +0200

    add O - Pyramid ICPC

---
+++ O - Pyramid/main.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class JavaApplication17 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1=sc.nextInt();
+        for (int i = 1; i <= num1; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+}
+}
