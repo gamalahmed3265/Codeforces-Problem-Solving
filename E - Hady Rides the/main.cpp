dbb1d1c8246f2656a12aa54e518c5d9b67e603d1
gamal ahmed <gamalahmed3265@gmail.com>
2023-04-08 00:25:12 +0200

    add E - Hady Rides the ICPC contest2

---
+++ E - Hady Rides the/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int n,row,col;
+    cin>>n;
+    row=n/4;
+    if (row%2==0)
+        col=n%4;
+    else
+        col=3-(n%4);
+    cout<<row<<" "<<col<<endl;
+
+}
