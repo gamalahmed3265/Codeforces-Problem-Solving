af61ff11453915779bd96c18bcc5aa087a0f07d0
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-10-14 09:32:25 +0300

    Update main.cpp

--- sheet 4 R. String Score.cpp/main.cpp
+++ sheet 4 R. String Score.cpp/main.cpp
@@ -1,45 +1,59 @@
-#include<bits/stdc++.h>
+
+#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
+
 int main()
 {
-		int size;
-		cin>>size;
-		string value; // "VWY"   ['Z','Y','0','Y','Z']
-		cin>>value;
-		int score = 0;
-		for(int i=0;i<value.size();i++){ // i = 0
-			char c = value[i]; // Z
-			switch(c){
-				case 'V':
-					score += 5;
-					break;
-				case 'W': 
-				    score += 2;
-				    break;
-				case 'Z': 
-				if(i != value.size() - 1){
-					if(value[i + 1] == 'W'){
-				    	score /= 2;
-				    	value[i+1] = '0'; // 48
-					} else if (value[i + 1] == 'V'){
-						score /= 5;
-						value[i+1] = '0'; // 48
-					}
-				}
-					break;
-				case 'Y':
-					if(i != value.size() - 1){
-					value.push_back(value[i+1]);
-					value[i+1] = '0';
-					
-					}
-					break;
-				case 'X':
-					if(i != value.size() - 1){
-					value[i+1] = '0';
-				}
-					
-			}
-		}
-		cout<<score;
+  int n ;cin>>n;
+  string s; cin>>s;
+  int score = 0 ;
+  for(int i=0 ; i<s.size() ; i++)
+  {
+      char c = s[i];
+      if(c =='V')
+      {
+          score += 5;
+      }
+      else if(c == 'W')
+      {
+        score += 2;  
+      }
+      else if(c == 'Z')
+      {
+          if(i != s.size() -1)
+          {
+              if(s[i+1] == 'W')
+              {
+                score /= 2;
+                s[i+1] = 'a';
+              }
+              else if(s[i+1] == 'V')
+              {
+                  score /= 5;
+                  s[i+1] = 'a';
+              }
+          }
+      }
+      else if(c == 'Y')
+      {
+          if(i != s.size() -1)
+          {
+              s.push_back(s[i+1]);
+              s[i+1] = 'a';
+          }
+      }
+      else if (c == 'X')
+      {
+          if(i != s.size() -1)
+          {
+              s[i+1] = 'a';
+          }
+      }
+  }
+  cout<<score;
 }
+
+
+
