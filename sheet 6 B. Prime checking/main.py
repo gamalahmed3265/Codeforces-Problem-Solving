9454f79785cbb1e1841c91656424d7a907a0b164
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-10-28 09:32:56 +0200

    add Prime Checking

---
+++ sheet 6 B. Prime checking/main.py
@@ -0,0 +1,19 @@
+from math import sqrt
+
+
+def isPrime(n:int)->bool:
+
+	if (n <= 1):
+		return False
+
+	for i in range(2, int(sqrt(n))+1):
+		if (n % i == 0):
+			return False
+
+	return True
+
+n=int(input())
+if isPrime(n):
+    print("YES")
+else:
+    print("NO") 
